9aa7fde4d7e1c5c5d19db660b65b1d0864d6b56c
Nawaz Ali <fojiphoto161@gmail.com>
2025-07-25 18:15:01 +0500

    New Build

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,33 +1,29 @@
-const cacheName = "PinpinTeam-Golf Orbit-1.25.39";
-const contentToCache = [
-    "Build/9853637125e801e9aae48e78dbbdcfca.loader.js",
-    "Build/c5e3ce1a0242c704135be4d866484562.framework.js.unityweb",
-    "Build/453f9fec6de1898bfda92f3edc4b9fb5.data.unityweb",
-    "Build/f4ecbadad57c4ebc88341fe9350e277e.wasm.unityweb",
-    "TemplateData/style.css"
 
+const CACHE_NAME = 'Golf Orbit';
+const urlsToCache = [
+    '/',
+    '/index.html',
+    '/Build/f9080267fd9a4018f23134fac4227841.data.unityweb',
+    '/Build/002dcfd148e9a8d775a223990dfec544.wasm.unityweb',
+    '/Build/83401391d7620cf0e16fd2ee9a39ccd4.framework.js.unityweb',
+    '/Build/9853637125e801e9aae48e78dbbdcfca.loader.js',
+    '/TemplateData/style.css',
+    '/icon-1920x1080.png',
+    '/icon-512x512.png'
 ];
 
-self.addEventListener('install', function (e) {
-    console.log('[Service Worker] Install');
-    
-    e.waitUntil((async function () {
-      const cache = await caches.open(cacheName);
-      console.log('[Service Worker] Caching all: app shell and content');
-      await cache.addAll(contentToCache);
-    })());
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(function(cache) {
+            return cache.addAll(urlsToCache);
+        })
+    );
 });
 
-self.addEventListener('fetch', function (e) {
-    e.respondWith((async function () {
-      let response = await caches.match(e.request);
-      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
-      if (response) { return response; }
-
-      response = await fetch(e.request);
-      const cache = await caches.open(cacheName);
-      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
-      cache.put(e.request, response.clone());
-      return response;
-    })());
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches.match(event.request).then(function(response) {
+            return response || fetch(event.request);
+        })
+    );
 });
