5812a59bd0e7e086df1820b2a9f5297d15365710
Nawaz Ali <fojiphoto161@gmail.com>
2025-08-28 12:31:23 +0500

    Add files via upload

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -12,17 +12,17 @@ const urlsToCache = [
     '/icon-512x512.png'
 ];
 
-self.addEventListener('install', function(event) {
+self.addEventListener('install', function (event) {
     event.waitUntil(
-        caches.open(CACHE_NAME).then(function(cache) {
+        caches.open(CACHE_NAME).then(function (cache) {
             return cache.addAll(urlsToCache);
         })
     );
 });
 
-self.addEventListener('fetch', function(event) {
+self.addEventListener('fetch', function (event) {
     event.respondWith(
-        caches.match(event.request).then(function(response) {
+        caches.match(event.request).then(function (response) {
             return response || fetch(event.request);
         })
     );
